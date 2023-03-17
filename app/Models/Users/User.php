89a18d9d00a8f3334906fe5d11ef5ea5d093dc07
SatoMayu <hanon1742@gmail.com>
2023-03-17 11:33:52 +0900

    ログイン機能メモ

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -10,11 +10,19 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    // ↓Eloquent の create で割り当てようとする値は、あらかじめ Eloqunet 側で割り当て許可を与えなくてはならない。
+    // ↓ホワイトボックス方式…$fillable（代入可能）に配列を設定した上でprotectedすれば、「名前」「email」「パスワード」「役割」のみ書き換えることができる。
+    // ↓ブラックボックス方式…protect $guarded = ['id','year']とすると「id」「year」以外の要素を書き換えられる。
     protected $fillable = [
         'username',
         'email',
         'password',
         'admin_role',
+    ];
+
+    // ↓表示したくない要素がある際に、$hiddenを指定することで前もってデータを取得しないようにする。
+    protected $hidden = [
+        'password', 'remember_token',
     ];
 
     public function posts()
